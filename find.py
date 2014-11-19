af9b1b5719c736996c515fac7b534300a5b0e171
alhimkot <alhimkot@gmail.com>
2014-11-19 22:00:04 +0300

    Update find.py

--- find.py
+++ find.py
@@ -4,6 +4,7 @@
 import webbrowser
 
 # ГЛАВНОЕ МЕНЮ
+# поисковик будет заменен на duckduckGo, яндекс перенесу в поиск людей.
 def main():
 	print("1 Find 2 People 3 Info")
 	kay4main = input("kay: ")
@@ -41,7 +42,59 @@ def winFind():
 		else:
 			url = "http://yandex.ru/yandsearch?text=" + slovo2find
 			webbrowser.open(url)
-# ПОИСК ЛЮДЕЙ (ближайщая три дня)
+# ПОИСК ЛЮДЕЙ (ближайщие три дня)
+def winPeople():
+    print("╔---------------------------------------------------------------╗")
+    print("║(1) Друзья                                (2)          Новости ║")
+    print("║(3) Сообщества                            (4)      Аудиозаписи ║")
+    print("║(5) Видеозаписи                           (6)          Маркеры ║") 
+    print("║(7) Поиск через поисковик                 (8)            назад ║")
+    print("╚---------------------------------------------------------------╝")
+
+
+    vk=input(" Где искать? Введите: ")
+
+
+    if vk == "1":
+        chel=input(" Кого вы хотите найти?: ") 
+        url = "http://vk.com/search?c[name]=1&c[q]="+chel+"&c[section]=people"
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
+
+	
+    elif vk == "2":
+        new=input(" Что вас интересует?: ") 
+        url = "http://vk.com/search?c[q]="+new+"&c[section]=statuses" 
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
+    
+	
+    elif vk == "3":
+        com=input(" Введите название сообщества?: ") 
+        url = "http://vk.com/search?c[q]="+com+"&c[section]=communities" 
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
+    
+	
+    elif vk == "4":
+        aud=input(" Название песни или исполнителя: ") 
+        url = "http://vk.com/search?c[q]="+aud+"&c[section]=audio" 
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
+    
+	
+    elif vk == "5":
+        vid=input(" Что будем смотреть?: ") 
+        url = "http://vk.com/search?c[q]="+vid+"&c[section]=video" 
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
+    
+	
+    elif vk == "6":
+        mar=input(" Что вас интересует?: ") 
+        url = "http://vk.com/feed?q=%23"+mar+"&section=search" 
+        webbrowser.open(url) 
+        print("-----------------------------------------------------------------")
 # ПОИСК РЕШЕНИЙ (ближайшая неделя)
 
 while True: main()
